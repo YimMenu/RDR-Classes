bfae1e6ef1a89e4c1dc155b07731abebeafcb2fc
maybegreat48 <email@hostname>
2023-08-26 13:50:11 -0400

    feat(network): add some more stuff

--- network/CNetGamePlayer.hpp
+++ network/CNetGamePlayer.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include "netPlayer.hpp"
 
+class CPlayerInfo;
 class CNetGamePlayer : public rage::netPlayer
 {
 public:
-	char pad_0128[2440]; //0x0128
+	CPlayerInfo* m_PlayerInfo; //0x0128
+	char pad_0130[2432]; //0x0130
 }; //Size: 0x0AB0
 static_assert(sizeof(CNetGamePlayer) == 0xAB0);
--- network/netPlayer.hpp
+++ network/netPlayer.hpp
@@ -3,10 +3,23 @@
 
 namespace rage
 {
+	class rlGamerInfo;
 	class netPlayer
 	{
 	public:
-		virtual ~netPlayer() = default; // 0x00
+		virtual ~netPlayer() = default;                                   // 0x00
+		virtual void Initialize(void* unk, int message_id, int unk2) {};  // 0x08
+		virtual void Initialize2(int unk) {};                             // 0x10
+		virtual void Reset() {};                                          // 0x18
+		virtual bool IsValid() { return false; };                         // 0x20
+		virtual const char* GetName() { return nullptr; };                // 0x28
+		virtual void* GetUnk() { return nullptr; };                       // 0x30
+		virtual void SetTeam(int new_team) {};                            // 0x38
+		virtual bool IsSameTeam(netPlayer* other) { return false; };      // 0x40
+		virtual void _0x48() {};                                          // 0x48 some posse stuff
+		virtual void RefreshData() {};                                    // 0x50
+		virtual bool IsHost() { return true; };                           // 0x58
+		virtual rage::rlGamerInfo* GetGamerInfo() { return nullptr; };    // 0x60
 
 		char pad_0008[12]; //0x0008
 		uint32_t m_MessageId; //0x0014
@@ -14,7 +27,9 @@ namespace rage
 		uint8_t m_PlayerIndex; //0x0019
 		char pad_001A[10]; //0x001A
 		uint32_t m_JoinTime; //0x0024
-		char pad_0028[256]; //0x0028
+		char pad_0028[4]; //0x0028
+		uint32_t m_Team; //0x002C
+		char pad_0030[248]; //0x0030
 	}; //Size: 0x0128
 	static_assert(sizeof(rage::netPlayer) == 0x128);
 }
