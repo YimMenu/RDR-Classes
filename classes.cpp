69e4a1a0dca9be5b6d91b7240848ca414b6c6c76
maybegreat48 <email@hostname>
2023-10-01 19:52:56 -0400

    feat(netsync): add CPlayerAppearanceData

--- classes.cpp
+++ classes.cpp
@@ -5,6 +5,7 @@
 #include "entity/fwEntity.hpp"
 #include "network/sync/object/CObjectCreationData.hpp"
 #include "network/sync/ped/CPedCreationData.hpp"
+#include "network/sync/player/CPlayerAppearanceData.hpp"
 #include "network/sync/CProjectBaseSyncDataNode.hpp"
 #include "network/sync/netSyncDataNode.hpp"
 #include "network/sync/netSyncNodeBase.hpp"
