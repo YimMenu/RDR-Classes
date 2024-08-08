dad5278b9ffc7d43d8534b74e892f6ac38ec2c72
Rxann <80224521+Rxann@users.noreply.github.com>
2024-08-07 23:00:00 -0400

    Fix up class

--- network/sync/projectile/CProjectileCreationData.hpp
+++ network/sync/projectile/CProjectileCreationData.hpp
@@ -1,30 +1,18 @@
 #pragma once
-#include <cstdint>
 #include "rage/vector.hpp"
+#include <cstdint>
 
-struct CProjectileCreationData
-{
-	uint16_t unk_0000;
-	uint16_t m_OwnerObjectId;
-	uint32_t m_AmmoHash;
-	uint32_t m_WeaponHash;
-	uint32_t unk_000C;
-	rage::vector3 m_Vector;
-	uint32_t unk_0020;
-	uint16_t m_ObjectId2;
-	char pad_0026[2];
-	bool unk_0028;
-	char pad_0029[3];
-	uint32_t unk_002C;
-	bool pad_0030;
-	bool unk_0031;
-	char pad_0032[2];
-	float unk_0034;
-	bool unk_0038;
-	bool unk_0039;
-	bool unk_003A;
-	bool unk_003B;
-	char pad_003C[4];
-	rage::matrix34 m_Matrix;
+
+struct CProjectileCreationData {
+  char m_Pad1[2];
+  uint16_t m_OwnerObjectId;
+  uint32_t m_AmmoHash;
+  uint32_t m_WeaponHash;
+  char m_Pad2[4];
+  rage::vector3 m_Velocity;
+  char m_Pad3[4];
+  uint16_t m_ObjectId2;
+  char m_Pad4[26];
+  rage::matrix34 m_InitialPosition;
 };
 static_assert(sizeof(CProjectileCreationData) == 0x80);
